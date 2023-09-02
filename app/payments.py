f5f3e4c1f2822b44870f7cca24f72cf9f51bac1d
mukminpitoyo <mpitoyo.2020@scis.smu.edu.sg>
2023-09-03 00:53:29 +0800

    updates

--- app/payments.py
+++ app/payments.py
@@ -109,17 +109,17 @@ def makePayment(MembershipRecordId):
         db.session.add(payment)
         db.session.commit()
 
-        # Update the MembershipRecord by extending the End Date by 1 month, but keeping the same day e.g. from 2021-01-15 to 2021-02-15. And if the MembershipRecord's is anything but "Active", change it to "Active"
+        # Update the MembershipRecord by extending the End Date by 1 month, but keeping the same day e.g. from 2021-01-15 to 2021-02-15. And if the current date is less than the MembershipRecord's new End Date, we change the MembershipRecord's ActiveStatus to "Active"
         membershipRecord = MembershipRecord.query.filter_by(MembershipRecordId=MembershipRecordId).first()
         membershipRecord.EndDate = membershipRecord.EndDate + relativedelta(months=1)
-        if membershipRecord.ActiveStatus != "Active":
+        if datetime.now().date() < membershipRecord.EndDate:
             membershipRecord.ActiveStatus = "Active"
         db.session.commit()
 
         # Create a new Membership Log entry to record the extension of the membership
         membershipLog = MembershipLog(
             Date=datetime.now().date(),
-            Description="Membership extended by 1 month",
+            Description="Membership extended by 1 month to {}".format(membershipRecord.EndDate.strftime("%d/%m/%Y")),
             ActionType="Membership Extended",
             MembershipRecordId=MembershipRecordId
         )
@@ -139,37 +139,109 @@ def makePayment(MembershipRecordId):
             }
         ), 500
     
+# Function and Route to make a yearly Payment for a MembershipRecord
+@app.route('/payments/yearly/membershiprecord/<int:MembershipRecordId>', methods=['POST'])
+def makeYearlyPayment(MembershipRecordId):
+    """
+    Sample Request (if any other mode than PayPal is used)
+    {
+        "PayPalId": null,
+        "Amount": 2400,
+        "Discount": 0,
+        "PaymentMode": "PayNow"
+    }
+    Sample Request (if PayPal is used)
+    {
+        "PayPalId": "1234567890",
+        "Amount": 2400,
+        "Discount": 0,
+        "PaymentMode": "PayPal"
+    }
+    """
+    data = request.get_json()
+    payment = Payment(
+        PayPalId = data['PayPalId'],
+        MembershipRecordId = MembershipRecordId,
+        TransactionDate = datetime.now(),
+        Amount = data['Amount'],
+        Discount = data['Discount'],
+        PaymentMode = data['PaymentMode']
+    )
+    try:
+        db.session.add(payment)
+        db.session.commit()
+
+        # Update the MembershipRecord by extending the End Date by 1 year, but keeping the same day e.g. from 2021-01-15 to 2022-01-15. And if the current date is less than the MembershipRecord's new End Date, we change the MembershipRecord's ActiveStatus to "Active"
+        membershipRecord = MembershipRecord.query.filter_by(MembershipRecordId=MembershipRecordId).first()
+        membershipRecord.EndDate = membershipRecord.EndDate + relativedelta(years=1)
+        if datetime.now().date() < membershipRecord.EndDate:
+            membershipRecord.ActiveStatus = "Active"
+        db.session.commit()
+
+        # Create a new Membership Log entry to record the extension of the membership
+        membershipLog = MembershipLog(
+            Date=datetime.now().date(),
+            Description="Membership extended by 1 year to {}".format(membershipRecord.EndDate.strftime("%d/%m/%Y")),
+            ActionType="Membership Extended",
+            MembershipRecordId=MembershipRecordId
+        )
+        db.session.add(membershipLog)
+        db.session.commit()
+
+        return jsonify(
+            payment.json()
+        ), 201
+
+    except:
+        return jsonify(
+            {
+                "code": 400,
+                "message": "An error occurred creating the payment.",
+                "error": True
+            }
+        ), 400
+    
 # Function and Route to refresh the ActiveStatus of all Membership Records by checking the Payment Date and Membership Record End Date
 @app.route("/membershiprecord/refresh")
 def refreshMembershipRecords():
-    # First, get a list of all Membership Types that are Monthly
-    monthlyMembershipList = Memberships.query.filter_by(Type="Monthly").all()
+    # First, get a list of all Membership Records
+    membershipRecordList = MembershipRecord.query.all()
 
     # Save the current date
     currentDate = datetime.now().date()
 
-    # Then, using the Membership Type ID of this monthlyMembershipList, get a list of all Membership Records that are Monthly
-    monthlyMembershipRecordList = MembershipRecord.query.filter(MembershipRecord.MembershipTypeId.in_([membership.MembershipTypeId for membership in monthlyMembershipList])).all()
-
-    # Using this monthlyMembershipRecordList, for each Membership Record, check if the membership record has expired by using the current date and the Membership Record's End Date. 
-    for membershipRecord in monthlyMembershipRecordList:
-        # IF Membership Record's End Date is before or the same as the current date, then first check the latest Payment made for this Membership Record.  
-        if membershipRecord.EndDate <= currentDate:
+    # Using this membershipRecordList, for each Membership Record, check if the membership record has expired by using the current date and the Membership Record's End Date. 
+    for membershipRecord in membershipRecordList:
+        # IF Membership Record's End Date is more than 3 days before the current date, then first check the latest Payment made for this Membership Record. 
+        # !!!(This is used when the user have not made payment even after the 3 days grace period)!!! 
+        if membershipRecord.EndDate < currentDate - timedelta(days=3):
             latestPayment = Payment.query.filter_by(MembershipRecordId=membershipRecord.MembershipRecordId).order_by(Payment.TransactionDate.desc()).first()
-            # IF the latest Payment Date is more than 3 days before the current date, then update the Membership Record's ActiveStatus to "Expired - Payment Overdue"
-            if latestPayment.TransactionDate < currentDate - timedelta(days=3):
+            # Change the MembershipRecord's ActiveStatus to "Expired - Payment Overdue" if the latest Payment's Transaction Date is more than 3 days before the Membership Record's End Date
+            if latestPayment.TransactionDate < membershipRecord.EndDate - timedelta(days=3):
                 membershipRecord.ActiveStatus = "Expired - Payment Overdue"
                 db.session.commit()
-                # Create a new Membership Log with "Expired - Payment Overdue" status
+                # Create a new Membership Log with "Expired - Payment Overdue" status, and a description of "Membership record expired on dd/mm/yyyy - payment overdue" where dd/mm/yyyy is the Membership Record's End Date
                 membershipLog = MembershipLog(
                     Date=currentDate,
-                    Description="Membership record expired - payment overdue",
+                    Description="Membership record expired on {} - payment overdue".format(membershipRecord.EndDate.strftime("%d/%m/%Y")),
                     ActionType="Expired - Payment Overdue",
                     MembershipRecordId=membershipRecord.MembershipRecordId
                 )
                 db.session.add(membershipLog)
                 db.session.commit()
-    
+        # Else if the Membership Record's End Date is the same or before the current date, update the MembershipRecord's ActiveStatus to "Membership Expired - Please make payment by dd/mm/yyyy to renew" where dd/mm/yyyy is 3 days after the Membership Record's End Date 
+        # !!!(This is used to give the user a 3 day grace period to make payment)!!!
+        elif membershipRecord.EndDate <= currentDate:
+            membershipRecord.ActiveStatus = "Membership Expired - Please make payment by {} to renew".format((membershipRecord.EndDate + timedelta(days=3)).strftime("%d/%m/%Y"))
+            db.session.commit()
+
+        # Else if the Membership Record is about to expire in 7 days (by using the current date and the Membership Record's End Date), update the MembershipRecord's ActiveStatus to "Membership Expiring - Please make payment by dd/mm/yyyy to renew" where dd/mm/yyyy is the Membership Record's End Date
+        # !!!(This is used to give the user an early reminder to make payment before membership expires)!!!
+        elif membershipRecord.EndDate <= currentDate + timedelta(days=7):
+            membershipRecord.ActiveStatus = "Active (Membership Expiring - Please make payment by {} to renew)".format(membershipRecord.EndDate.strftime("%d/%m/%Y"))
+            db.session.commit()
+
+        
     # Return a success message
     return jsonify(
         {
